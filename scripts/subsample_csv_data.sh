227502e8c6d182d7260be415fe08ba7b52eee4de
Kai Blumberg <kai@dhcp-10-142-196-0.uawifi.arizona.edu>
2018-11-03 16:21:20 -0700

    added python script to subsample a desired max number of rows from the csv files and .sh script to run it

---
+++ scripts/subsample_csv_data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#
+# Run "subsample_csv_data.py" for each of the biome.csv files downloaded form EBI_MGnify
+# Author: Kai Blumberg <kblumberg@email.arizona.edu>
+#
+
+# subsample at most 50 csv rows from the low resolution taxonomic data
+./subsample_csv_data.py -i ../metagenomes/dataset_1/ -o ../metagenomes/dataset_1_subsample -n 50
+
+# subsample at most 38 csv rows from the high resolution taxonomic data
+./subsample_csv_data.py -i ../metagenomes/dataset_2/ -o ../metagenomes/dataset_2_subsample -n 38

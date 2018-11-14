d5cae8cd44a6206fe4be57e2bc3d18069b0e58e9
Kai Blumberg <kai@dhcp-10-142-248-144.uawifi.arizona.edu>
2018-11-14 09:36:10 -0700

    modified the bash scripts to work on both datasets

--- scripts/get-go-terms.sh
+++ scripts/get-go-terms.sh
@@ -3,27 +3,30 @@
 #
 # For each CSV file in the "metagenomes" dir, get the associated GO terms
 # Author: Ken Youens-Clark <kyclark@email.arizona.edu>
+# Modifications: Kai Blumberg <kblumberg@email.arizona.edu>
 #
 
 set -u
 
-METAGENOMES_DIR="../metagenomes/all"
-GO_DIR="../data/go"
-MAX=100
+# Get go data from the from the subsampled low resolution taxonomic data, dataset 1
 
-if [[ ! -d "$METAGENOMES_DIR" ]]; then
-    echo "Missing expected METAGENOMES_DIR \"$METAGENOMES_DIR\""
+METAGENOMES_DIR_1="../metagenomes/dataset_1_subsample"
+GO_DIR_1="../data/go_1"
+MAX=10
+
+if [[ ! -d "$METAGENOMES_DIR_1" ]]; then
+    echo "Missing expected METAGENOMES_DIR_1 \"$METAGENOMES_DIR_1\""
     exit 1
 fi
 
-METAGENOMES=$(mktemp)
-find "$METAGENOMES_DIR" -type f > "$METAGENOMES"
+METAGENOMES_1=$(mktemp)
+find "$METAGENOMES_DIR_1" -type f > "$METAGENOMES_1"
 
 while read -r FILE; do
     METAGENOME=$(basename "$FILE" ".csv")
     echo "Extracting accessions from \"$METAGENOME\""
 
-    OUT_DIR="$GO_DIR/$METAGENOME"
+    OUT_DIR="$GO_DIR_1/$METAGENOME"
 
     [[ ! -d "$OUT_DIR" ]] && mkdir -p "$OUT_DIR"
 
@@ -38,10 +41,50 @@ while read -r FILE; do
         FILE=$(basename "$URL")
         printf "%3d: %s\n" $i "$URL"
         (cd "$OUT_DIR" && wget --no-clobber "$URL")
-        if [[ $i -gt $MAX ]]; then
+        if [[ $i -eq $MAX ]]; then
             break
         fi
     done < "$ACCS"
-done < "$METAGENOMES"
+done < "$METAGENOMES_1"
+
+# Get go data from the from the subsampled low resolution taxonomic data, dataset 1
+# Note we are using verison 2.0 of the EBI_pipeline for this dataset
+
+METAGENOMES_DIR_2="../metagenomes/dataset_2_subsample"
+GO_DIR_2="../data/go_2"
+MAX=10
+
+if [[ ! -d "$METAGENOMES_DIR_2" ]]; then
+    echo "Missing expected METAGENOMES_DIR_2 \"$METAGENOMES_DIR_2\""
+    exit 1
+fi
+
+METAGENOMES_2=$(mktemp)
+find "$METAGENOMES_DIR_2" -type f > "$METAGENOMES_2"
+
+while read -r FILE; do
+    METAGENOME=$(basename "$FILE" ".csv")
+    echo "Extracting accessions from \"$METAGENOME\""
+
+    OUT_DIR="$GO_DIR_2/$METAGENOME"
+
+    [[ ! -d "$OUT_DIR" ]] && mkdir -p "$OUT_DIR"
+
+    ACCS=$(mktemp)
+    awk -F',' 'NR>1 {print $1 "\t" $7}' "$FILE" | sed 's/"//g'  > "$ACCS"
+
+    i=0
+    while read -r ACC ERR; do
+        i=$((i+1))
+        #URL="https://www.ebi.ac.uk/metagenomics/api/v1/analyses/${ACC}/file/${ERR}_FASTA_InterPro.tsv.gz"
+        URL="https://www.ebi.ac.uk/metagenomics/api/v1/analyses/${ACC}/file/${ERR}_MERGED_FASTQ_GO.csv"
+        FILE=$(basename "$URL")
+        printf "%3d: %s\n" $i "$URL"
+        (cd "$OUT_DIR" && wget --no-clobber "$URL")
+        if [[ $i -eq $MAX ]]; then
+            break
+        fi
+    done < "$ACCS"
+done < "$METAGENOMES_2"
 
 echo "Done."
--- scripts/merge_go.sh
+++ scripts/merge_go.sh
@@ -3,14 +3,24 @@
 #
 # Run "merge_go.py" for each of the frequency matrices by metagenome
 # Author: Ken Youens-Clark <kyclark@email.arizona.edu>
+# Modifications by Kai Blumberg
 #
 
-IN_DIR="../data/go"
-OUT_DIR="../data/freqs"
+IN_DIR="../data/go_1"
+OUT_DIR="../data/freqs_1"
 
 for DIR in $IN_DIR/*; do
     echo $DIR
     ./merge_go.py -i "$DIR" -o "$OUT_DIR/$(basename "$DIR").csv"
 done
+
+IN_DIR="../data/go_2"
+OUT_DIR="../data/freqs_2"
+
+for DIR in $IN_DIR/*; do
+    echo $DIR
+    ./merge_go.py -i "$DIR" -o "$OUT_DIR/$(basename "$DIR").csv"
+done
+
 
 echo "Done."
